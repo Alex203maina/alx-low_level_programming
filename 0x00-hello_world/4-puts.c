fd85987b70a5c1eba6adb47627b319296bc67dc0
alex203maina <alex203maina@gmail.com>
2023-10-06 01:24:09 -0700

    it nice

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual puzzle
+ * puzzle, followed by a new line"
+ * Return 0.
  */
 int main(void)
 {
