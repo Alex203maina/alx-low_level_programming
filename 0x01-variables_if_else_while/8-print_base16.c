e94a24341269b6acbaa26583a2328c8a0bb2f670
alex203maina <alex203maina@gmail.com>
2023-10-07 10:39:36 -0700

    sorry

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Prints all single digit numbers of base 10
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char hexDigit;
+    
+	for (hexDigit = '0'; hexDigit <= '9'; hexDigit++)
+	{
+		putchar(hexDigit);
+	}
+	for (hexDigit = 'a'; hexDigit <= 'f'; hexDigit++)
+	{
+		putchar(hexDigit);
+	}
+	putchar('\n');
+	return (0);
+}
+
