bcd1c1cbe9be98aa0df2b1de0bcbdd5e05746ee3
alex203maina <alex203maina@gmail.com>
2023-10-07 10:17:54 -0700

    kamosh

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Displays base 10 numbers
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int number;
+	for (number = 0; number <= 9; number++)
+	{
+		putchar(number + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
