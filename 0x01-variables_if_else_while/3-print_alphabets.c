3f50d278fc562a22b750e6c72a25dc2b036fa99f
alex203maina <alex203maina@gmail.com>
2023-10-07 11:00:50 -0700

    hello

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Prints lower and uppercase alphabets
+* Return: Always 0 (success)
+*/
+	int main(void)
+{
+	char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+	    putchar(letter);
+    }
+    for (letter = 'A'; letter <= 'Z'; letter++)
+    {
+	    putchar(letter);
+    }
+    putchar('\n');
+return (0);
+}
+
